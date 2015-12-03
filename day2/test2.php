4f33bc3e4c00cf0d4685a1c0ced0ef23dfebe117
Chris Cornutt <enygma@phpdeveloper.org>
2015-12-03 08:17:57 -0600

    solving day 2, test 2

--- day2/test2.php
+++ day2/test2.php
@@ -1,3 +1,25 @@
 <?php
 
 // Day 2, test 2
+
+$input = file(__DIR__.'/input2.txt');
+$total = 0;
+
+foreach ($input as $line) {
+  $line = trim($line);
+  if (empty($line)) {
+    continue;
+  }
+  $parts = explode('x', $line);;
+  sort($parts);
+  
+  list($height, $width, $length) = $parts;
+
+  $presentWrap = ($width * 2) + ($height * 2);
+  $presentBow = $width * $height * $length;
+
+  $result = ($presentWrap + $presentBow);
+  $total += $result;
+}
+
+echo 'RESULT: '.$total."\n";
